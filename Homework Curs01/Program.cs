e07b22e4a755b36c19e2bd7461363751a41d7d0a
FlorinSerban231 <florinserban231@yahoo.ro>
2018-05-21 08:51:16 +0300

    Modoficat

--- Homework Curs01/Program.cs
+++ Homework Curs01/Program.cs
@@ -10,6 +10,28 @@ namespace Homework_Curs01
     {
         static void Main(string[] args)
         {
+                Random randomNumber = new Random();
+                intguess = randomNumber.Next(1, 100);
+
+                Console.WriteLine("Introduceti un mumar intre 1 si 100!");
+            tryAgain: int input = int.Parse(str);
+                Console.WriteLine(nr)
+    
+            while (input!= guess)
+                {
+                    if (input < guess)
+                    {
+                        Console.WriteLine("Numarul introdus este prea mic. Mai incearca!");
+                        goto tryAgain;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Numarul introdus este prea mare. Mai incearca!");
+                        goto tryAgain;
+                    }
+                }
+                Console.WriteLine("Ati ghicit Numarul!");
+                Console.ReadKey();
+            }
         }
     }
-}
